3651adfa97a49dfccdad64ec91b1db68ac2b2f2f
Kogiuncovered TV <your-email@example.com>
2026-03-01 19:33:14 +0100

    Fix: increase lambda size to 50mb, update wsgi

--- kogiuncovered/wsgi.py
+++ kogiuncovered/wsgi.py
@@ -4,3 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kogiuncovered.settings')
 
 application = get_wsgi_application()
+
+# Vercel requires the entrypoint to be named 'app'
+app = application
