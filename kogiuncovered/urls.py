2d211135e238dbbcdc6f8bb0f95b4d8a0f566ca2
Kogiuncovered TV <your-email@example.com>
2026-03-02 09:40:56 +0100

    Add ads.txt route at root domain

--- kogiuncovered/urls.py
+++ kogiuncovered/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.views.decorators.cache import cache_page
+
+
+def ads_txt(request):
+    """Serve ads.txt file"""
+    content = "google.com, pub-4211041071990839, DIRECT, f08c47fec0942fa0"
+    return HttpResponse(content, content_type='text/plain')
+
 
 urlpatterns = [
+    path('ads.txt', ads_txt, name='ads_txt'),
     path('', include('blog.urls')),
 ]
 
