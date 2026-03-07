5f89eb51aff06c3b501eb1136adf3d082ba18fec
Kogiuncovered TV <your-email@example.com>
2026-03-07 06:23:17 +0100

    Move sw.js to static folder and add URL route for root access

--- kogiuncovered/urls.py
+++ kogiuncovered/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.views.decorators.cache import cache_page
+from django.views.static import serve
+import os
 
 
 def ads_txt(request):
@@ -12,8 +14,17 @@ def ads_txt(request):
     return HttpResponse(content, content_type='text/plain')
 
 
+def service_worker(request):
+    """Serve sw.js file from static directory"""
+    sw_path = os.path.join(settings.BASE_DIR, 'static', 'sw.js')
+    with open(sw_path, 'r') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='application/javascript')
+
+
 urlpatterns = [
     path('ads.txt', ads_txt, name='ads_txt'),
+    path('sw.js', service_worker, name='service_worker'),
     path('', include('blog.urls')),
 ]
 
